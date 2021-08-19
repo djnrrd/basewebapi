1253bacc51c8fb97f4fb851f9397371165f0aa42
DJ Nrrd <djnrrd@gmail.com>
2021-08-19 13:08:01 +0100

    Update setup.py
    Update version number due to mis-match with work managed version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='basewebapi_python',
-      version='0.2.0',
+      version='0.3.0',
       install_requires=['requests'],
       author='Ralph Taylor',
       author_email='djnrrd@gmail.com',
