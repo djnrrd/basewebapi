066f835c654dfe5770364da1a7c74be8e60bd5ff
DJ Nrrd <djnrrd@gmail.com>
2021-12-11 12:16:43 +0000

    Added build structure ready for PyPi

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,17 +12,18 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../../src/'))
+from basewebapi.conf import VERSION
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'basewebapi'
-copyright = '2021, Ralph Taylor'
-author = 'Ralph Taylor'
+copyright = '2021, DJ Nrrd'
+author = 'DJ Nrrd'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = VERSION
 
 
 # -- General configuration ---------------------------------------------------
@@ -48,7 +49,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- setup.py
+++ setup.py
@@ -1,16 +1,10 @@
-#!/usr/bin/env python
+from setuptools import setup
+import os
+import sys
+sys.path.extend([os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'src')])
+from basewebapi.conf import VERSION
 
-from distutils.core import setup
-
-setup(name='basewebapi_python',
-      version='0.3.2',
-      install_requires=['requests, aiohttp'],
-      author='Ralph Taylor',
-      author_email='djnrrd@gmail.com',
-      url='https://github.com/djnrrd/basewebapi',
-      description='Basic model for expanding when writing web api wrappers',
-      long_description='Basic model for expanding when writing web api '
-                       'wrappers',
-      license='GPL3',
-      packages=['basewebapi']
+setup(
+    version=VERSION,
 )
